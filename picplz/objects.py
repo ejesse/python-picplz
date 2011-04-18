0e870c0f75d867fbc0233bb808a2530d1800d3b2
Jesse Emery <jesse@onagertech.com>
2011-04-18 16:27:49 -0400

    can now get users and pics, pics have cities and places

--- picplz/objects.py
+++ picplz/objects.py
@@ -2,10 +2,9 @@ import datetime
 from picplz.errors import PicplzError
 import simplejson
 
-class PicplzObject():
+class PicplzObject(object):
     api = None
     
-    @classmethod
     def map(self, api, data):
         """map a dict object into a model instance."""
         raise NotImplementedError
@@ -26,9 +25,8 @@ class PicplzImageFile(PicplzObject):
     width = 0
     height = 0
     
-    @classmethod
     def map(self, api, data):
-        """map a JSON object into a model instance."""
+        """map a dict object into a model instance."""
         self.api=api
         self.name=data['name']
         self.url=data['img_url']
@@ -94,16 +92,36 @@ class Pic(PicplzObject):
     city=None
     place=None
     
-    @classmethod
     def map(self, api, data):
         """map a JSON object into a model instance."""
-        self.view_count = int(data['view_count'])
-        self.url = data['url']
-        self.caption = data['caption']
-        self.comment_count = data['comment_count']
-        self.like_count = data['like_count']
-        self.id = int(data['id'])
-#        self.date = datetime.datetime(data['date'])
+        try:
+            self.view_count = int(data['view_count'])
+        except:
+            pass
+        try:
+            self.url = data['url']
+        except:
+            pass
+        try:
+            self.caption = data['caption']
+        except:
+            pass
+        try:
+            self.comment_count = data['comment_count']
+        except:
+            pass
+        try:
+            self.like_count = data['like_count']
+        except:
+            pass
+        try:
+            self.id = int(data['id'])
+        except:
+            pass
+        try:
+            self.date = datetime.datetime.fromtimestamp(data['date'])
+        except:
+            pass
         pic_files = {}
         pic_files_dict = {}
         for key in data['pic_files'].keys():
@@ -113,8 +131,11 @@ class Pic(PicplzObject):
             pic_files_dict['width'] = data['pic_files'][key]['width'] 
             pic_files[pic_files_dict['name']] = PicplzImageFile().from_dict(api, pic_files_dict)
         self.pic_files = pic_files
-        creator_data = data['creator']
-        self.creator = PicplzUser.from_dict(api, creator_data)
+        try:
+            creator_data = data['creator']
+            self.creator = PicplzUser.from_dict(api, creator_data)
+        except:
+            pass
         try:
             city_data = data['city']
             self.city = PicplzCity.from_dict(api, city_data)
@@ -123,7 +144,7 @@ class Pic(PicplzObject):
             pass
         try:
             place_data = data['place']
-            #self.place = PicplzPlace.from_dict(self, api, place_data)
+            self.place = PicplzPlace.from_dict(api, place_data)
         except:
             ## no place, no biggie
             pass
@@ -146,9 +167,13 @@ class PicplzUser(PicplzObject):
     follower_count = 0
     id = None
     icon = None
-    pics = []
+    pics = None
+    __has_more_pics__=False
+    __last_pic_id__=False
     
-    @classmethod
+    def __init__(self,api=None):
+        self.api = api
+    
     def map(self, api, data):
         self.api = api
         self.username = data['username']
@@ -156,6 +181,9 @@ class PicplzUser(PicplzObject):
         self.follower_count = data['follower_count']
         self.following_count = data['following_count']
         self.id = int(data['id'])
+        self.pics = {}
+        __has_more_pics__=False
+        __last_pic_id__=False
         icon_data = data['icon']
         pic_files_dict = {}
         pic_files_dict['name'] = 'icon'
@@ -166,29 +194,42 @@ class PicplzUser(PicplzObject):
         try:
             pics_data = data['pics']
             for pic_data in pics_data:
-                print pic_data
+                next_pic = Pic()
                 next_pic = Pic.from_dict(self.api,pic_data)
-                print next_pic
-                self.pics.append(next_pic)
+                self.pics[next_pic.id] = next_pic
         except:
-            ## no pics
+            ## no pics :(
             pass
 
     def __to_string__(self):
         return "%s <%s>" % (self.display_name, self.username)
 
-    def get_pics(self):
+    def __fetch_pics__(self,go_back_for_more=False,last_pic_id=None):
         if not self.api:
             raise PicplzError("Objects must have been instantiated with no API. Without an API the object cannot fetch anything :(")
-        if len(self.pics) < 1:
-            pics_user = self.api.get_user(username=self.username,include_pics=True)
-            self.pics = pics_user.pics
+        if go_back_for_more:
+            # NomNomNomNomNomNomNomNomNom
+            pics_user = self.api.get_user(username=self.username,include_pics=True,last_pic_id=last_pic_id,pic_page_size=100)
+        else:
+            pics_user = self.api.get_user(username=self.username,include_pics=True,last_pic_id=last_pic_id)
+        for pic_key in pics_user.pics.keys():
+            self.pics[pic_key] = pics_user.pics[pic_key]
+            self.pics[pic_key].creator = self
+        if go_back_for_more:
+            if pics_user.__has_more_pics__:
+                #recursion FTW
+                # NomNomNomNomNomNomNomNomNom
+                return self.__fetch_pics__(go_back_for_more=True,last_pic_id=pics_user.__last_pic_id__)
         return self.pics
-            
+
+    def fetch_pics(self):
+        return self.__fetch_pics__()
         
+    def fetch_all_pics(self):
+        return self.__fetch_pics__(go_back_for_more=True)
 
     def from_dict(api,data):
-        new_object = PicplzUser()
+        new_object = PicplzUser(api=api)
         new_object.map(api,data)
         return new_object
         
@@ -200,7 +241,24 @@ class PicplzComment(PicplzObject):
     pass
 
 class PicplzPlace(PicplzObject):
-    pass
+    url = None
+    id = None
+    name = None
+    
+    def map(self, api, data):
+        self.url = data['url']
+        self.id = int(data['id'])
+        self.name = data['name']
+    
+    def __to_string__(self):
+        return self.name
+
+    def from_dict(api,data):
+        new_object = PicplzPlace()
+        new_object.map(api,data)
+        return new_object
+        
+    from_dict = staticmethod(from_dict)
     
 class PicplzCity(PicplzObject):
     
@@ -208,7 +266,6 @@ class PicplzCity(PicplzObject):
     id = None
     name = None
     
-    @classmethod
     def map(self, api, data):
         self.url = data['url']
         self.id = int(data['id'])
